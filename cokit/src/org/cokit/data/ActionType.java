de40cd930db1a95092eef9360d670d0cebb00c1a
Dash <alchemistmark@126.com>
2017-03-03 12:21:49 +0800

    clear & precise

--- cokit/src/org/cokit/data/ActionType.java
+++
@@ -1,8 +0,0 @@
-package org.cokit.data;
-
-// the operation type that cokit supports, other type will be ignored by server
-
-public enum ActionType {
-	LOGIN, BROADCAST, SYNCHRONIZATION, EXPERIMENTRESULT,OPEN,FETCH,
-	SERVERPERFORMANCE
-}
--- cokit/updateJAR.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/sh
-
-mvn dependency:copy-dependencies -DoutputDirectory=WebContent/WEB-INF/lib/ -DincludeScope=runtime
